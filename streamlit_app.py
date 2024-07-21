6db8d04d6c10e0904cc1f7a535a8fd69e8c631a2
romikgosai <romikgosai2057@gmail.com>
2024-07-21 21:24:10 +0545

    leaderboard index fixing

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,12 +45,17 @@ def show_leaderboard():
     if category == "Classification":
         csv_file = 'classification_leaderboard.csv'
         leaderboard = pd.read_csv(csv_file)
-        leaderboard = leaderboard.sort_values(by='F1 score', ascending=False)
+        leaderboard = leaderboard.sort_values(by='F1 score', ascending=False).reset_index(drop=True)
+        leaderboard.reset_index(drop=True, inplace=True)
+        leaderboard.index += 1
+        st.table(leaderboard)
     else:
         csv_file = 'regression_leaderboard.csv'
         leaderboard = pd.read_csv(csv_file)
         leaderboard = leaderboard.sort_values(by='r2 score', ascending=False)
-    st.table(leaderboard)
+        leaderboard.reset_index(drop=True, inplace=True)
+        leaderboard.index += 1
+        st.table(leaderboard)
 
 # Main app
 def main():
